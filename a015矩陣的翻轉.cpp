bbaa4ad15dd53a7fe921f143c49c482b12e59de0
1130200-clover <s1130200@dmhs.kh.edu.tw>
2026-04-28 11:28:55 +0800

    Update a015矩陣的翻轉.cpp

--- a015矩陣的翻轉.cpp
+++ a015矩陣的翻轉.cpp
@@ -3,7 +3,7 @@
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : 多維陣列，函式建構
 //============================================================================
 
 
