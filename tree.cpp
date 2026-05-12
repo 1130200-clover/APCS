a3d978dee34a59692aa65e856d7db1166d909a13
1130200-clover <s1130200@dmhs.kh.edu.tw>
2026-05-12 11:05:54 +0800

    Update tree.cpp

--- tree.cpp
+++ tree.cpp
@@ -3,7 +3,7 @@
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
-// Description : tree in C++, Ansi-style
+// Description : 多層巢狀迴圈
 //============================================================================
 
 #include <iostream>
