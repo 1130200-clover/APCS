a48b5ac289ed184da15a2ac5017f3e5f9c04f798
1130200-clover <s1130200@dmhs.kh.edu.tw>
2026-05-12 11:05:08 +0800

    Update a006 一元二次方程式.cpp

--- a006 一元二次方程式.cpp
+++ a006 一元二次方程式.cpp
@@ -3,7 +3,7 @@
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
-// Description : 一元二次方程式 in C++, Ansi-style
+// Description : 函式建構 in C++, Ansi-style
 //============================================================================
 
 #include <iostream>
